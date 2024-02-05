9f72e3352ef3a4e841a73de618c1b90b46b000b5
Tboules <tboules@gmail.com>
2024-02-05 14:39:30 -0800

    auth handlers

--- internal/server/auth-handler.go
+++ internal/server/auth-handler.go
@@ -1,10 +1,48 @@
 package server
 
 import (
-	egoth "github.com/nabowler/echo-gothic"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func (s *Server) ProviderAuthCallbackHandler(c echo.Context) error {
+func (s *Server) AuthProviderCallbackHandler(c echo.Context) error {
+	user, err := s.auth.CompleteUserAuth(c)
 
-	user, err := echogothic.CompleteUserAuth(c)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println(user)
+
+	return nil
+	// return c.Redirect(http.StatusTemporaryRedirect, "/")
+}
+
+func (s *Server) AuthHandler(c echo.Context) error {
+	gothUser, err := s.auth.CompleteUserAuth(c)
+
+	if err == nil {
+		fmt.Printf("user already exists: %v", gothUser)
+		return nil
+	} else {
+		err := s.auth.BeginAuthHandler(c)
+		if err != nil {
+			return err
+		}
+
+		return nil
+		// return c.Redirect(http.StatusTemporaryRedirect, "/")
+	}
+}
+
+func (s *Server) LogoutHandler(c echo.Context) error {
+	err := s.auth.Logout(c)
+	if err != nil {
+		return err
+	}
+
+	return c.Redirect(http.StatusTemporaryRedirect, "/")
 }
