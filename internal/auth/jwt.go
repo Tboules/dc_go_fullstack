031e0dc72ade32b6c441e2e7cda1a023e114e5d1
Tboules <tboules@gmail.com>
2024-02-06 11:32:20 -0800

    jwt service

---
+++ internal/auth/jwt.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type UserClaims struct {
+	ProviderId string `json:"id"`
+	jwt.StandardClaims
+}
+
+func (a *Auth) NewAccessToken(claims UserClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SIGNING_SECRET")))
+}
+
+func (a *Auth) NewRefreshToken(claims jwt.StandardClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SIGNING_SECRET")))
+}
+
+func (a *Auth) ParseAccessToken(token string) (*UserClaims, error) {
+	parsedAccessToken, err := jwt.ParseWithClaims(token, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SIGNING_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedAccessToken.Claims.(*UserClaims), nil
+}
+
+func (a *Auth) ParseRefreshToken(token string) (*jwt.StandardClaims, error) {
+	parsedRefreshToken, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SIGNING_SECRET")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedRefreshToken.Claims.(*jwt.StandardClaims), nil
+}
