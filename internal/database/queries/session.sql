ede008e662df7cc4aeb36206173139f24910fbe4
Tboules <tboules@gmail.com>
2024-02-12 11:12:05 -0800

    sqlc db functions for sessions

--- internal/database/queries/session.sql
+++ internal/database/queries/session.sql
@@ -1,1 +1,14 @@
+-- name: GetSession :one
+SELECT * FROM `session`
+WHERE token = ? LIMIT 1;
 
+-- name: SaveSession :execresult
+INSERT INTO `session` (
+  token, user_id, expires_at
+) VALUES (
+  ?, ?, ?
+);
+
+-- name: DeleteSession :exec
+DELETE FROM `session`
+WHERE token = ?;
